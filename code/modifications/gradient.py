afdde5464ae5ce00227bef572b4eaa542b1b4820
worbit <bernhard@arch.ethz.ch>
2018-08-06 13:42:46 +0200

    modify gradient

--- code/modifications/gradient.py
+++ code/modifications/gradient.py
@@ -1,8 +1,12 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
 """
-gradient vectors from voxelspace
+Calculates a gradient direction vector for every cell.
+    Inputs:
+        vals: distance field values
+        nx: number of cells in x direction
+        ny: number of cells in y direction
+        nz: number of cells in z direction
+    Output:
+        a: a 3d vector in gradient direction for every cell
 """
 
 __author__     = ['Mathias Bernhard']
@@ -10,43 +14,25 @@ __copyright__  = 'Copyright 2018 / Digital Building Technologies DBT / ETH Zuric
 __license__    = 'MIT License'
 __email__      = '<bernhard@arch.ethz.ch>'
 
-import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 
-nyz = ny*nz
-nxy = nx*ny
+a = []
 
-print nx,ny,nz
+nyz = ny*nz
 
 def get_index(x,y,z):
-    return x * nyz + y * nz + z
-
-a = []
-b = []
+    return x*nyz + y*nz + z
 
 for x in range(nx):
     for y in range(ny):
-        for z in range(nz//2,nz//2+1):
-            b.append(rg.Vector3d(3*x-100,3*y-100,3*z-100))
+        for z in range(nz):
             if x==0 or x==nx-1 or y==0 or y==ny-1 or z==0 or z==nz-1:
                 a.append(rg.Vector3d(0,0,0))
             else:
-                ix = get_index(x,y,z)
-                v = vals[ix]
-
-                xx = get_index(x+1,y,z)
-                vx = vals[xx]
-                xx = get_index(x-1,y,z)
-                ux = vals[xx]
-
-                yy = get_index(x,y+1,z)
-                vy = vals[yy]
-                yy = get_index(x,y-1,z)
-                uy = vals[yy]
-
-                zz = get_index(x,y,z+1)
-                vz = vals[zz]
-                zz = get_index(x,y,z-1)
-                uz = vals[zz]
-
-                a.append(rg.Vector3d(vy-uy,-1*(vx-ux),vz-uz))
+                ux = vals[get_index(x-1,y,z)]
+                vx = vals[get_index(x+1,y,z)]
+                uy = vals[get_index(x,y-1,z)]
+                vy = vals[get_index(x,y+1,z)]
+                uz = vals[get_index(x,y,z-1)]
+                vz = vals[get_index(x,y,z+1)]
+                a.append(rg.Vector3d(vx-ux,vy-uy,vz-uz))
